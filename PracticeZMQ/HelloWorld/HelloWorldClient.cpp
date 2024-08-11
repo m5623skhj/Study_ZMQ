d7dd30247e97e9a5bab610fba98d8fe5bfb63574
m5623skhj <m5623skhj@gmail.com>
2024-08-11 14:31:46 +0900

    * REQ, REP 테스트 케이스 추가

--- PracticeZMQ/HelloWorld/HelloWorldClient.cpp
+++ PracticeZMQ/HelloWorld/HelloWorldClient.cpp
@@ -2,23 +2,65 @@
 #include <string>
 #include <iostream>
 
+/*
+2. 하나의 클라이언트가 recv 2회 이상 불가능 : 크래시 발생
+*/
+void TestCase2(zmq::socket_t& requester)
+{
+	std::string first = "fir";
+	std::string second = "sec";
+
+	{
+		zmq::message_t req(first.begin(), first.end());
+		std::cout << "Send first " << std::endl;
+		auto result = requester.send(req, zmq::send_flags::none);
+		if (result.has_value() == false)
+		{
+			std::cout << "result not has value with first" << std::endl;
+			return;
+		}
+	}
+
+	{
+		zmq::message_t req(second.begin(), second.end());
+		std::cout << "Send second " << std::endl;
+		auto result = requester.send(req, zmq::send_flags::none);
+		if (result.has_value() == false)
+		{
+			std::cout << "result not has value with second" << std::endl;
+			return;
+		}
+	}
+}
+
+bool DefaultCase(zmq::socket_t& requester, std::string hello, int reqNum)
+{
+	zmq::message_t req(hello.begin(), hello.end());
+	std::cout << "Sending Hello " << reqNum << std::endl;
+	requester.send(req, zmq::send_flags::none);
+
+	zmq::message_t reply(0);
+	requester.recv(reply);
+	std::cout << "Received World " << reqNum << std::endl;
+
+	return true;
+}
+
 int main()
 {
 	zmq::context_t context(1);
 	std::cout << "Connection to server" << std::endl;
 	zmq::socket_t requester(context, ZMQ_REQ);
 	requester.connect("tcp://localhost:5555");
+	// TestCase 3 : 라운드로빈 테스트
+	// 5555 -> 5556 -> 5555 -> 5556 -> ... 이런 식으로 정상 동작함
+	requester.connect("tcp://localhost:5556");
 
 	const std::string hello = "Hello";
 	for(int reqNum = 0; reqNum < 10; ++reqNum)
 	{
-		zmq::message_t req(hello.begin(), hello.end());
-		std::cout << "Sending Hello " << reqNum << std::endl;
-		requester.send(req, zmq::send_flags::none);
-
-		zmq::message_t reply(0);
-		requester.recv(reply);
-		std::cout << "Received World " << reqNum << std::endl;
+		DefaultCase(requester, hello, reqNum);
+		//TestCase2(requester);
 	}
 
 	requester.close();
--- PracticeZMQ/HelloWorld/HelloWorldServer.cpp
+++ PracticeZMQ/HelloWorld/HelloWorldServer.cpp
@@ -3,23 +3,63 @@
 #include <iostream>
 #include <Windows.h>
 
+/*
+1. server 가 먼저 SEND 불가능 : 크래시 발생
+*/
+bool TestCase1(zmq::socket_t& socket, int loopSize)
+{
+	std::cout << "Take " << loopSize << std::endl;
+	zmq::message_t reply(5);
+	memcpy((void*)reply.data(), "World", 5);
+	auto result = socket.send(reply, zmq::send_flags::none);
+	if (result.has_value())
+	{
+		std::cout << "Send failed " << loopSize << std::endl;
+		return false;
+	}
+
+	return true;
+}
+
+bool TestCase2(zmq::socket_t& socket)
+{
+	zmq::message_t req(0);
+	socket.recv(req);
+	std::cout << "Recived" << std::endl;
+
+	return true;
+}
+
+bool DefaultCase(zmq::socket_t& socket)
+{
+	zmq::message_t req(0);
+	socket.recv(req);
+	std::cout << "Recived Hello" << std::endl;
+
+
+	zmq::message_t reply(5);
+	memcpy((void*)reply.data(), "World", 5);
+	socket.send(reply, zmq::send_flags::none);
+
+	return true;
+}
+
 int main()
 {
 	zmq::context_t context(1);
 	zmq::socket_t socket(context, ZMQ_REP);
 	socket.bind("tcp://*:5555");
 
+	int i = 0;
 	while (true)
 	{
-		zmq::message_t req(0);
-		socket.recv(req);
-		std::cout << "Recived Hello" << std::endl;
+		++i;
+
+		DefaultCase(socket);
+		//TestCase1(socket, i);
+		//TestCase2(socket);
 
 		Sleep(1);
-
-		zmq::message_t reply(5);
-		memcpy((void*)reply.data(), "World", 5);
-		socket.send(reply, zmq::send_flags::none);
 	}
 
 	socket.close();
